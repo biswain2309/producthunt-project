2d30da2d05759a398505023117e33bba1370c5a9
Indrani Biswas <indranibiswas@Indranis-MacBook-Air.local>
2020-02-21 22:19:21 +0530

    Added Null values to foreign key

--- products/models.py
+++ products/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Product(models.Model):
-    title = models.CharField(max_length=200)
-    url = models.TextField(max_length=200)
+    title = models.CharField(max_length=255)
+    url = models.TextField()
     pub_date = models.DateTimeField()
     votes = models.IntegerField(default=1)
     image = models.ImageField(upload_to='images/')
     icon = models.ImageField(upload_to='images/')
     body = models.TextField()
-    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+    hunter = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
